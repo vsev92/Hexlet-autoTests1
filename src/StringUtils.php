6568dc910e066bdf38ad1ae057aeb4986aa2af10
Vsevolod Petrov <vsev92@mail.ru>
2024-09-09 15:18:36 +0700

    all

---
+++ src/StringUtils.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace StringUtils;
+
+function capitalize($text)
+{
+    $firstSymbol = mb_strtoupper($text[0]);
+    $restSubstring = mb_substr($text, 1);
+    return "{$firstSymbol}{$restSubstring}";
+}
---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests;
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+use Webmozart\Assert\Assert;
+use StringUtils;
+
+if (StringUtils\capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (StringUtils\capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
